445268fd21c57bd4c761b756ec4f97de59a80af4
Brandon Ramcharitar <122882645+BrandyRumRum@users.noreply.github.com>
2024-11-17 21:39:08 -0400

    Updated ChatBotGeneratorTest.java

--- automarker/src/test/java/ChatBotGeneratorTest.java
+++ automarker/src/test/java/ChatBotGeneratorTest.java
@@ -1,5 +1,8 @@
+package project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 public class ChatBotGeneratorTest {
 
